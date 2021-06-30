bc8f1668bb172119e26bf9daa849ea3a30a82341
Alper Tayfun <tayfun.alper@gmail.com>
2021-06-30 16:22:19 +0300

    test

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -21,13 +21,13 @@ const farms: FarmConfig[] = [
     pid: 1,
     lpSymbol: 'NYAN2-BNB LP',
     lpAddresses: {
-      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
-      56: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+      97: '0xC970D43d330d8d591796Bd93d42af9D65c271313',
+      56: '0xC970D43d330d8d591796Bd93d42af9D65c271313',
     },
     tokenSymbol: 'NYAN2',
     tokenAddresses: {
-      97: '0xa7438fc5c3e4c6c476ef2e5de851c2ecaab20883',
-      56: '0x45b07DF4FE00202dd31F2D95f6C06f3A029625D5',
+      97: '0xA7438fC5c3E4C6C476Ef2E5de851c2ECAAb20883',
+      56: '0xA7438fC5c3E4C6C476Ef2E5de851c2ECAAb20883',
     },
     quoteTokenSymbol: QuoteToken.BNB,
     quoteTokenAdresses: contracts.wbnb,
